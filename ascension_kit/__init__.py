3131512517a4c273e802b2fc872c2095d5721a1e
zomgreg <greg.moselle@enstratius.com>
2015-07-23 11:07:38 -0500

    bumping version

--- ascension_kit/__init__.py
+++ ascension_kit/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'ascension_kit'
-__version__ = '0.0.13'
+__version__ = '0.0.14'
 __author__ = 'Greg Moselle'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2015'
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requires = ['beautifulsoup4>=4.3.2', 'prettytable==0.7.2']
 
 setup(
     name="ascension_kit",
-    version='0.0.13',
+    version='0.0.14',
     author="Greg Moselle",
     author_email="zomgreg+ascensionkit@gmail.com",
     description="A utility to report ascension information from NAO.",
